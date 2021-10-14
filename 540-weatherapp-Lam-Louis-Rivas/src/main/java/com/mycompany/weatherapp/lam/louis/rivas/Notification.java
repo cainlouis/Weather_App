d719e4a676439de4c3ad0613efce71ac4873f7dc
Nael Louis <nael.louis1002@gmail.com>
2021-10-14 18:12:14 -0400

    Changed confirmation for warning

--- 540-weatherapp-Lam-Louis-Rivas/src/main/java/com/mycompany/weatherapp/lam/louis/rivas/Notification.java
+++ 540-weatherapp-Lam-Louis-Rivas/src/main/java/com/mycompany/weatherapp/lam/louis/rivas/Notification.java
@@ -9,33 +9,30 @@ import java.util.Optional;
  */
 public class Notification {
     /**
-     * Create a confirmation dialog and display the string it gets as an input
-     * @param message contains the message asked to the user to confirm
-     * @return return the user response
-     */
-    public boolean confirmationDialog(String message) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Confirmation Dialog");
-        alert.setHeaderText("Confirmation needed.");
-        alert.setContentText(message);
-
-        Optional<ButtonType> result = alert.showAndWait();
-        return result.get() == ButtonType.OK;
-    }
-    
-    /**
-     * Create an error dialog and display the string it gets as an input
+     * Create an warning dialog and display the string it gets as an input
      * @param message contains the cause of the error an the action the user 
      * should follow 
      */
-    public void errorDialog(String message) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
+    public void warningDialog(String message) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Warning Dialog");
+        alert.setHeaderText("Warning!");
+        alert.setContentText(message);
+
+        alert.showAndWait();
+    }
+    
+    /**
+     * Create an warning dialog and display the string it gets as an input
+     * @param message contains the cause of the error an the action the user 
+     * should follow 
+     */
+    public void informationDialog(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Information Dialog");
         alert.setHeaderText("For your information!");
         alert.setContentText(message);
 
         alert.showAndWait();
     }
-
-
 }
